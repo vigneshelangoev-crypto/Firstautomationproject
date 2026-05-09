e8da047069cc5178fc407563f66e3b23ccd1efb9
Vignesh E <vigneshelangoev@gmail.com>
2026-05-09 22:01:44 +0530

    remote commit

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,6 +13,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +22,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -30,6 +34,7 @@ public class BaseClass {
     public static WebDriver driver;
     public Logger logger;
     public Properties p;
+    public DesiredCapabilities cap;
 
     @BeforeClass(alwaysRun = true)
     @Parameters({"browser","headless"})
@@ -41,7 +46,18 @@ public class BaseClass {
         logger=LogManager.getLogger(this.getClass());
         boolean isHeadless= Boolean.parseBoolean(hl);
 
-        
+        if(p.getProperty("execution_evn").equalsIgnoreCase("remote"))
+        {
+        String huburl="http://13.51.220.79:4444/wd/hub";
+        cap= new DesiredCapabilities();
+        cap.setPlatform(Platform.LINUX);
+        // browser
+        cap.setBrowserName(br.toLowerCase());
+        driver= new RemoteWebDriver(new URL(huburl),cap);
+        }
+
+        if(p.getProperty("execution_evn").equalsIgnoreCase("local"))
+        {
         switch(br.toLowerCase())
         {
         case "chrome": 
@@ -65,6 +81,7 @@ public class BaseClass {
         driver= new FirefoxDriver(fOptions); break;
         default : System.out.println("Invalid Browser"); return;
         }
+    }
 
         //driver= new ChromeDriver();
 
